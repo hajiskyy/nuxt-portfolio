60b6740a3edb2dbd7d6f99055bf9e2a943aab44a
Haji Mohammed <hajisky07@gmail.com>
2024-06-17 19:53:34 +0100

    structure changes

--- app.vue
+++ app.vue
@@ -1,7 +1,15 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="w-full relative">
+    <Header />
+    <HeroAnimation />
+    <UContainer class="overflow-x-hidden">
+      <HomeContentNav />
+      <UContainer class="md:max-w-xl lg:max-w-4xl mr-0 px-0 pb-5">
+        <NuxtPage />
+      </UContainer>
+      <Footer />
+    </UContainer>
+  </div>
 </template>
 
 <script setup>
